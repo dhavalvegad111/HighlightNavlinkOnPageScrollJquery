554101a3c9cea0e67f5fd9463bda3ff9110719b6
Dhaval Vegad <96942545+dhavalvegad111@users.noreply.github.com>
2024-05-08 19:57:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // highlight navlink on page scroll
 $(document).ready(function () {
-  $('header ul.navmenu a[href*=#]').bind('click', function (e) {
+  $('header ul a[href*=#]').bind('click', function (e) {
     e.preventDefault();
     var target = $(this).attr("href");
     $('html, body').stop().animate({
@@ -15,8 +15,8 @@ $(window).scroll(function () {
   var scrollDistance = $(window).scrollTop();
   $('section').each(function (i) {
     if ($(this).position().top <= scrollDistance) {
-      $('header ul.navmenu a.active').removeClass('active');
-      $('header ul.navmenu a').eq(i).addClass('active');
+      $('header ul a.active').removeClass('active');
+      $('header ul a').eq(i).addClass('active');
     }
   });
 }).scroll();
